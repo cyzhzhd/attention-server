778b73fa372c604768e76dea1e510fc70cafcb13
Jae Gwon Choi <cyrojyro@gmail.com>
2020-10-02 22:13:49 +0900

    feat: serve ai model

--- dist/app.js
+++ dist/app.js
@@ -6,20 +6,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
 var express_1 = __importDefault(require("express"));
 var morgan_1 = __importDefault(require("morgan"));
+var path_1 = __importDefault(require("path"));
 var user_1 = __importDefault(require("./routers/user"));
 var class_1 = __importDefault(require("./routers/class"));
 var session_1 = __importDefault(require("./routers/session"));
 var errorHandler_1 = require("./helpers/errorHandler");
 var app = express_1.default();
 app.use(morgan_1.default("dev"));
-app.use(morgan_1.default('common', {
-    stream: fs_1.default.createWriteStream('./access.log', { flags: 'a' })
+app.use(morgan_1.default("common", {
+    stream: fs_1.default.createWriteStream("./access.log", { flags: "a" }),
 }));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use('/user', user_1.default);
-app.use('/class', class_1.default);
-app.use('/session', session_1.default);
+app.use("/download", express_1.default.static(path_1.default.join(__dirname, "../static")));
+app.use("/user", user_1.default);
+app.use("/class", class_1.default);
+app.use("/session", session_1.default);
 app.use(errorHandler_1.undefinedMethodHandler);
 app.use(errorHandler_1.errorHandler);
 module.exports = app;
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
 import fs from "fs";
 import express from "express";
 import logger from "morgan";
+import path from "path";
 import userRouter from "./routers/user";
 import classRouter from "./routers/class";
 import classSessionRouter from "./routers/session";
-import { undefinedMethodHandler, errorHandler } from './helpers/errorHandler'
+import { undefinedMethodHandler, errorHandler } from "./helpers/errorHandler";
 
 const app = express();
 
 app.use(logger("dev"));
-app.use(logger('common', {
-    stream: fs.createWriteStream('./access.log', { flags: 'a' })
-}));
+app.use(
+  logger("common", {
+    stream: fs.createWriteStream("./access.log", { flags: "a" }),
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/user', userRouter);
-app.use('/class', classRouter);
-app.use('/session', classSessionRouter);
+app.use("/download", express.static(path.join(__dirname, "../static")));
+
+app.use("/user", userRouter);
+app.use("/class", classRouter);
+app.use("/session", classSessionRouter);
 
 app.use(undefinedMethodHandler);
 app.use(errorHandler);
